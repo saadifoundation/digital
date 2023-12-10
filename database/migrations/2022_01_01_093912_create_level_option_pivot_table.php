1abe04b193092528e15d7bbde49cf6b52ff9626d
Amir Mojiri <amojiry@gmail.com>
2023-12-10 14:35:39 +0200

    Fix pivot migration class names

--- database/migrations/2022_01_01_093912_create_level_option_pivot_table.php
+++ database/migrations/2022_01_01_093912_create_level_option_pivot_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class LevelOptionPivotTable extends Migration
+class CreateLevelOptionPivotTable extends Migration
 {
     /**
      * Run the migrations.
